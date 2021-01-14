970544ccb8da70e80d8cfadbc933af1ec8aca55a
ayoub <ayoubbenhamou0731@gmail.com>
2021-01-14 12:49:39 +0100

    start

---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "mainHeader.h"
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ src/mainHeader.h
@@ -0,0 +1,8 @@
+#ifndef MOVESQUARE_MAINHEADER_H
+#define MOVESQUARE_MAINHEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL.h>
+
+#endif //MOVESQUARE_MAINHEADER_H
